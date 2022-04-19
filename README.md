20a20d63ac5355d7bb3597474bfe22ed32b0c779
usingnamespacestc <usingnamespacestc@gmail.com>
2022-04-19 17:04:17 -0400

    Front end demo finished

--- README.md
+++ README.md
@@ -1,4 +1,19 @@
 # OOFinalFront
-This repo is the front end. [Direct me to the back end.](https://github.com/usingnamespacestc/OOFinalBack)
+This repo is the front end. <del>[Direct me to the back end.](https://github.com/usingnamespacestc/OOFinalBack)</del>
 
-Using Vue.js 2.6.14 and elementui 2.15.7
+[Demo Link](https://kod-va.unstc.site:54321/data/User/admin/home/self-introduction/OOFinalFront/)</del>
+
+## Library
+|Name|Version|
+|-|-|
+|[Vue.js](https://github.com/vuejs/vue)|2.6.14|
+|[elementui](https://github.com/ElementUI/lib)|2.15.7|
+|[echarts](https://github.com/apache/echarts)|4.3.0|
+
+## Usage
+* Login or Signup & Login first
+![image](img/login.png)
+* Fill all the informations
+![image](img/fill.png)
+* Calculate your COVID risk
+![image](img/calculate.png)
--- index.js
+++ index.js
@@ -3,9 +3,53 @@ var app = new Vue({
     data(){
         return{
             dataTest: "data test",
-            username: "",
-            password: "",
-            currentPage: "login"
+            currentPage: "login", //dashboard
+            username: "sample_user",
+            password: "sample_password",
+            activeName: "age",
+            age: 0,
+            genderMale: false,
+            symptoms: [],
+            options: [{
+              value: 'fatigue',
+              label: 'fatigue'
+            }, {
+              value: 'sore throat',
+              label: 'sore throat'
+            }, {
+              value: 'fever',
+              label: 'fever'
+            }, {
+              value: 'loss of smell or taste',
+              label: 'loss of smell or taste'
+            }],
+            vaccineStatus: "",
+            vaccineOptions: ["Not Vaccined", "Vaccined", "Fully Vaccined"],
+            feeling: null,
+            risk: {
+                tooltip: {
+                  formatter: '{a} <br/>{b} : {c}%'
+                },
+                series: [
+                  {
+                    name: 'COVID',
+                    type: 'gauge',
+                    progress: {
+                      show: true
+                    },
+                    detail: {
+                      valueAnimation: true,
+                      formatter: '{value}'
+                    },
+                    data: [
+                      {
+                        value: 50,
+                        name: 'RISK'
+                      }
+                    ]
+                  }
+                ]
+            }
         }
     },
     methods:{
@@ -20,6 +64,66 @@ var app = new Vue({
         },
         switchToDashboard(){
             this.currentPage = "dashboard"
+            // this.$nextTick(_ =>{
+            //     console.log("next");
+            //     this.calculateRisk()
+            // });
+        },
+        handleClick(tab, event){
+          // console.log(tab, event);
+        },
+        goToGender(){
+          this.activeName = "gender"
+        },
+        goToSymptoms(){
+          this.activeName = "symptoms"
+        },
+        goToVaccineStatus(){
+          this.activeName = "vaccine status"
+        },
+        goToFeeling(){
+          this.activeName = "feeling"
+        },
+        goToRiskCalculator(){
+          this.calculateRisk()
+        },
+        calculateRisk(){
+            // calculate the risk
+            // this.risk["series"][0]["data"][0]["value"] = Math.round(Math.random() * 100)
+            // risk gender
+            if(this.genderMale){
+              this.risk["series"][0]["data"][0]["value"] *= 0.69
+            }
+            else{
+              this.risk["series"][0]["data"][0]["value"] *= 0.31
+            }
+            // risk age
+            this.risk["series"][0]["data"][0]["value"] = 50 + Math.abs(this.age - 25)
+            // risk symptoms
+            this.risk["series"][0]["data"][0]["value"] += this.symptoms.length * 15
+            // risk vaccine
+            if(this.vaccineStatus == "Not Vaccined"){
+              this.risk["series"][0]["data"][0]["value"] += 25
+            }
+            else if(this.vaccineStatus == "Vaccined"){
+              this.risk["series"][0]["data"][0]["value"] -= 25
+            }
+            else if(this.vaccineStatus == "Fully Vaccined"){
+              this.risk["series"][0]["data"][0]["value"] -= 50
+            }
+            // risk feeling
+            this.risk["series"][0]["data"][0]["value"] -= this.feeling * 10
+            // limit
+            if(this.risk["series"][0]["data"][0]["value"] < 0){
+              this.risk["series"][0]["data"][0]["value"] = 0
+            }
+            else if(this.risk["series"][0]["data"][0]["value"] > 100){
+              this.risk["series"][0]["data"][0]["value"] = 100
+            }
+            // put the chart
+            var chartDom = document.getElementById("riskChart")
+            var myChart = echarts.init(chartDom)
+            this.risk && myChart.setOption(this.risk)
         }
     }
 })
